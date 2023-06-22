06a8990010d0d8a00c0f458b1778a7e1461a93d5
Frokolll111 <buther1433@gmail.com>
2023-06-22 17:18:02 +0700

    Commit

---
+++ src/test/java/pro/sk/cours2/Cours2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sk.cours2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cours2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
